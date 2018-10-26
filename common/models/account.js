88749e0680623a206dd5462c63d59c0b7f624342
matt470 <e.m.singainy@gmail.com>
2018-10-26 12:39:34 +0400

    Résolution de l'inscription

--- common/models/account.js
+++ common/models/account.js
@@ -1,8 +1,5 @@
 'use strict';
 
 module.exports = function(Account) {
-    Account.beforeRemote('create', function(context, account, next) {
-        context.args.data.accountId = context.req.accessToken.userId;
-        next();
-      });
+    
 };
